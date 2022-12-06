89d38fe7850b379e626c64eabc7ea0254467b136
emil584c <emilwaellind@gmail.com>
2022-12-06 09:54:52 +0100

    .

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,27 +1,17 @@
 
 let moreText = document.getElementById("more");
 let box = document.getElementById("yes");
-
-
-// function readmore() {
-//     if(){
-        
-//     }else{
-        
-//     }
-// }
-
-// dots.style.display === "none"
-
-
+let boxe = document.getElementById("hydroponiBox2")
 
 box.addEventListener("click", function folding(){ 
     if(moreText.style.display = "none"){
         moreText.style.display = "flex";
-        box.style.height ="500px";
-        box.style.width ="500px";
+        // box.style.height ="500px";
+        // box.style.width ="500px";
+        boxe.style.display ="flex"
     }else{
         moreText.style.display = "none";
+        boxe.style.display ="flex"
     }
 });
 
