d73fc38d00d0b8226b9bcda9489f3f8eda911556
emil584c <emilwaellind@gmail.com>
2022-12-07 19:47:12 +0100

    forside

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,17 +1,50 @@
 
-let moreText = document.getElementById("more");
+// let moreText = document.getElementById("more");
+// let box = document.getElementById("yes");
+// let boxe = document.getElementById("hydroponiBox2")
+
+// box.addEventListener("click", function folding(){ 
+//     if(moreText.style.display = "none"){
+//         moreText.style.display = "flex";
+//         // box.style.height ="500px";
+//         // box.style.width ="500px";
+//         boxe.style.display ="flex"
+//     }else{
+//         moreText.style.display = "none";
+//         boxe.style.display ="flex"
+//     }
+// });
+
+
+
+
+
+
+let moreText = document.getElementById("info-box-1");
 let box = document.getElementById("yes");
-let boxe = document.getElementById("hydroponiBox2")
+let mainText = document.getElementById("info-main-text")
+let infoBox = document.getElementById("info-box")
 
 box.addEventListener("click", function folding(){ 
     if(moreText.style.display = "none"){
+        infoBox.style.height ="250px";
+        mainText.style.display ="none";
+        moreText.style.opacity = "100";
         moreText.style.display = "flex";
-        // box.style.height ="500px";
-        // box.style.width ="500px";
-        boxe.style.display ="flex"
-    }else{
-        moreText.style.display = "none";
-        boxe.style.display ="flex"
     }
 });
 
+
+
+const observer = new IntersectionObserver ((entries) =>{
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        } 
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
