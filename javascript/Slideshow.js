edc623e7bc62c5c3336b2c63d863abe05c30f122
YoungChop96 <105641588+YoungChop96@users.noreply.github.com>
2022-12-15 16:49:59 +0100

    Update Slideshow.js

--- javascript/Slideshow.js
+++ javascript/Slideshow.js
@@ -1,8 +1,7 @@
-let i = 0; 			// Start Point
-let images = [];	// Images Array
-let time = 3000;	// Time Between Switch
+let i = 0;
+let images = [];
+let time = 3000;	
 
-// Image List
 images[0] = "images/Hydrovertic-system-1.jpg";
 images[1] = "images/Hydrovertic-system-4.jpg";
 images[2] = "images/Hydrovertic-system-7.jpg";
@@ -10,24 +9,18 @@ images[3] = "images/Hydrovertic-system-5.jpg";
 
 console.log(images)
 
-// Change Image
 function slideImg() {
     document.slideshow.src = images[i];
 
-    // Check If Index Is Under Max
     if (i < images.length - 1) {
-        // Add 1 to Index
         i++;
     } else {
-        // Reset Back To O
         i = 0;
     }
 
-    // Run function every x seconds
     setTimeout("slideImg()", time);
 }
 
-// Run function when page loads
 window.onload = slideImg;
 
 
