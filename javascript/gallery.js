1052be6dc34efec0419c025c731b003877fecb61
Maltewj <maltewj@gmail.com>
2022-12-17 16:09:35 +0100

    alt tags på teknologi billeder

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -11,6 +11,7 @@ function showSlides(n, no) {
   let i;
   let x = document.getElementsByClassName(slideId[no]);
   if (n > x.length) {slideIndex[no] = 1}    
+  
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
