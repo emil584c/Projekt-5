06c29b8d5afbadb81117e075253a19a9467904c4
emil584c <emilwaellind@gmail.com>
2022-12-18 14:11:44 +0100

    image name fixes

--- javascript/Slideshow.js
+++ javascript/Slideshow.js
@@ -2,10 +2,10 @@ let i = 0;
 let images = [];
 let time = 3000;	
 
-images[0] = "images/Hydrovertic-system-1.jpg";
-images[1] = "images/Hydrovertic-system-4.jpg";
-images[2] = "images/Hydrovertic-system-7.jpg";
-images[3] = "images/Hydrovertic-system-5.jpg";
+images[0] = "images/hydrovertic-system-1.jpg";
+images[1] = "images/hydrovertic-system-4.jpg";
+images[2] = "images/hydrovertic-system-7.jpg";
+images[3] = "images/hydrovertic-system-5.jpg";
 
 console.log(images)
 
