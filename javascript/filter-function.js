4e19aa9d6fc8ed02f0fbe3b82ed741642f84eecc
emil584c <emilwaellind@gmail.com>
2022-12-18 19:14:59 +0100

    .

--- javascript/filter-function.js
+++ javascript/filter-function.js
@@ -77,8 +77,6 @@ console.log(btns)
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     if(this.style.display = "flex"){
-    let current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
     }
   });
